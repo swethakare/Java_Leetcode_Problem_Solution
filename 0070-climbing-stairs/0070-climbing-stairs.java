d1b583fb50f48c2d2795a62d2c1a8e85b282c4ee
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-02-25 14:39:06 -0500

    Time: 0 ms (100.00%), Space: 40 MB (92.02%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return helper(dp, n);
+    }
+
+    public int helper(int[] dp, int n) {
+        if (n == 0) {
+            dp[n] = 1; // There is one way to climb 0 stairs, i.e., do nothing.
+            return dp[n];
+        } else if (n == 1) {
+            dp[n] = 1; // There is one way to climb 1 stair, i.e., take one step.
+            return dp[n];
+        }
+        if (dp[n] == -1) {
+            dp[n] = helper(dp, n - 1) + helper(dp, n - 2);
+        }
+        return dp[n];
+    }
+}
