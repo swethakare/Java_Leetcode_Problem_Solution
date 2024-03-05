5f2fb41950a57a212550709b6068787df7b5a4ba
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-03-05 12:44:46 -0500

    Time: 155 ms (6.34%), Space: 45.6 MB (19.68%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -3,11 +3,13 @@ class Solution {
         int curend = 0, curfar = 0, jump=0;
         for(int i = 0; i < nums.length-1; i++){
             curfar = Math.max(curfar,i + nums[i]);
+            System.out.println("curfar"+curfar);
             if(i == curend){
                 jump++;
                 curend = curfar;
+                System.out.println("curend"+curend);
             }
-        
+            
         }
         return jump;
 
