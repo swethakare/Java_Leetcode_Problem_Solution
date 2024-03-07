c29fa4fb037171c2c9a7f6cb44418adc2ce9b658
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-03-07 15:09:27 -0500

    Time: 0 ms (100.00%), Space: 41.6 MB (31.08%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,28 +1,4 @@
-// class Solution {
-//     public int hIndex(int[] citations) {
-//     if(citations.length == 1){
-//         return 1;
-//     }
-//     Arrays.sort(citations);
-//     int mid = citations.length/2;
-//     int[] count = new int[citations[citations.length-1]+1]; 
-//     int cnt = 0;
-    
-//     for(int i = citations.length-1; i>=mid; i--){
-//         // check elements towards right;
-//         count[citations[i]]++;
-//         if(i < citations.length - 1){
-//             cnt++;
-//             count[citations[i]] = count[citations[i]] + cnt;
-//         }
-//         if(count[citations[i]] >= citations[i]){
-//             return citations[i];
-//         }
-//     }
-//     return -1;
-  
-//     }
-// }
+
 public class Solution {
 public int hIndex(int[] citations) {
     int[] arr = new int[citations.length + 1];
