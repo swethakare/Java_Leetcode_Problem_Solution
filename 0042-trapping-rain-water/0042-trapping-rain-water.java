e72ea4562eb3bcfce9f2bbb923a4495a9bc09b3d
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-03-10 18:42:58 -0400

    Time: 0 ms (100.00%), Space: 46.6 MB (13.40%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int leftmax = 0;
+        int rightmax = 0;
+        int totwater = 0;
+        while(left < right){
+            if(height[left] < height[right]){
+                if(height[left] >= leftmax){
+                    leftmax = height[left];
+                }else{
+                    totwater += leftmax - height[left];
+                }
+                left++;
+            }else{
+                if(height[right] >= rightmax){
+                    rightmax = height[right];
+                }else{
+                    totwater += rightmax - height[right];
+                }
+                right--;
+            }
+
+        }
+        return totwater;
+        
+    }
+}
