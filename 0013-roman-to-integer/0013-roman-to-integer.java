dbf084df61bd2cc2a2780f38f509c8c0377494d3
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-03-10 20:11:16 -0400

    Time: 47 ms (5.52%), Space: 44.3 MB (95.19%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,36 +1,45 @@
 class Solution {
     public int romanToInt(String s) {
-
-        HashMap<String,Integer> mp = new HashMap<String,Integer>();
-        mp.put("I",1);
-        mp.put("V",5);
-        mp.put("X",10);
-        mp.put("L",50);
-        mp.put("C",100);
-        mp.put("D",500);
-        mp.put("M",1000);
-        mp.put("CM",900);
-        mp.put("XC",90);
-        mp.put("IX",9);
-        mp.put("IV",4);
-        mp.put("XL",40);
-        mp.put("XC",90);
-        mp.put("CD",400);
-        
-        int sum = 0;
-        int i = 0;
-        int a = 0 ;
-        while(i < s.length()){
-            if(i+1 < s.length() && mp.containsKey(String.valueOf(s.substring(i,i+2)))){
-                a = mp.get(String.valueOf(s.substring(i,i+2)));
-                sum = sum + a;
-                i += 2;
-            }else{
-                a = mp.get(String.valueOf(s.charAt(i)));
-                sum = sum + a;
-                i++;
+        int nums[] = new int[s.length()];
+        for(int i = 0 ; i < s.length() ; i++){
+            switch(s.charAt(i)){
+                case 'I':
+                    nums[i] = 1;
+                    break;
+                case 'V':
+                    nums[i] = 5;
+                    break;
+                case 'X':
+                    nums[i] = 10;
+                    break;
+                case 'L':
+                    nums[i] = 50;
+                    break;
+                case 'C':
+                    nums[i] = 100;
+                    break;
+                case 'D':
+                    nums[i] = 500;
+                    break;
+                case 'M':
+                    nums[i] = 1000;
+                    break;
             }
         }
+        int sum = 0;
+        for(int i = 0 ; i < s.length()-1; i++){
+            if(nums[i] < nums[i+1]){
+                
+                sum -= nums[i];
+                 System.out.println(sum);
+            }else{
+                sum += nums[i];
+                 System.out.println(sum);
+            }
+        }
+       
+        sum += nums[nums.length-1];
         return sum;
+
     }
 }
