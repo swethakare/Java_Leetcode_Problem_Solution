4f02df380e13bf20361c3990ea0d7873686c4f9f
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-03-16 00:23:47 -0400

    Time: 3 ms (21.23%), Space: 41.3 MB (78.27%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,22 +1,30 @@
+import java.util.Arrays;
 
-public class Solution {
-public int hIndex(int[] citations) {
-    int[] arr = new int[citations.length + 1];
-    for(int i = 0 ;i < citations.length; i++){
-        if(citations[i] >= arr.length){
-            arr[arr.length-1]++;
-        }else{
-            arr[citations[i]]++;
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int[] arr = new int[citations.length+1];
+        int idx = 0;
+        for (int citation : citations) {
+            if (citation >= arr.length) {
+                idx = arr.length - 1;
+                arr[idx]++;
+            } else {
+                idx = citation;
+                arr[idx]++;
+            }
         }
-    }
-    int tot = 0;
-    for(int i = arr.length-1; i >= 0; i--){
-        tot = tot + arr[i];
-        if(tot >= i){
-            return i;
+
+        // Iterate over the counts array to find the h-index
+        int sum = 0;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sum += arr[i];
+            if (sum >= i) {
+                return i;
+            }
         }
+
+        // If no h-index is found, return 0
+        return 0;
     }
-    return -1;
 }
-}
-    
