d20312a12bb5370ff001b853d48287e8778127db
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-03-17 15:25:53 -0400

    Time: 11 ms (17.48%), Space: 46.5 MB (54.04%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,41 +1,34 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         ArrayList<ArrayList<Integer>> ar = new ArrayList<>();
-        // Arrays.sort(intervals, new Comparator<int[]>(){
-        //    public int compare(int[] a, int[] b){
-        //        if(a[0] != b[0]){
-        //            return a[0] - b[0];
-        //        }else{
-        //            return a[1] - b[1];
-        //        }
-        //    } 
-        // });
-        Arrays.sort(intervals, (a,b)->Integer.compare(a[0],b[0]));
-        int[][]ans = helper(intervals,ar);
-        return ans;
-    }
-    public int[][] helper(int[][] intervals, ArrayList<ArrayList<Integer>> ar){
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-        ar.add(new ArrayList<>(Arrays.asList(start,end)));
-        int j  = 0;
-        for(int i = 1; i < intervals.length; i++){
-            if(ar.get(j).get(1) >= intervals[i][0]){
-                end = ar.get(j).get(1);
-                ar.get(j).set(1,Math.max(end,intervals[i][1]));
-            }else{
-                start = intervals[i][0];
-                end = intervals[i][1];
-                ar.add(new ArrayList<>(Arrays.asList(start,end)));
-                j++;
+        ArrayList<Integer> row = new ArrayList<>();
+        Arrays.sort(intervals,Comparator.comparingInt(inter->inter[0]));
+        row.add(intervals[0][0]);
+        row.add(intervals[0][1]);
+        ar.add(row);
+        
+        for(int i = 1; i < intervals.length; i++) {
+            int start = ar.get(ar.size() - 1).get(0);
+            int end = ar.get(ar.size() - 1).get(1);
+            int nextstart = intervals[i][0];
+            int nextend = intervals[i][1];
+            
+            if(nextstart <= end) {
+                ar.get(ar.size() - 1).set(1,Math.max(end,nextend));
+            } else {
+                ArrayList<Integer> newRow = new ArrayList<>();
+                newRow.add(nextstart);
+                newRow.add(nextend);
+                ar.add(newRow);
             }
         }
-        int[][] ans = new int[ar.size()][ar.get(0).size()];
-        for(int i = 0; i<ar.size(); i++){
-            for(int k = 0; k<ar.get(0).size(); k++){
-                ans[i][k] = ar.get(i).get(k);
-            }
+        
+        int[][] result = new int[ar.size()][2];
+        for(int i = 0; i < ar.size(); i++) {
+            result[i][0] = ar.get(i).get(0);
+            result[i][1] = ar.get(i).get(1);
         }
-        return ans;
+        
+        return result;
     }
 }
