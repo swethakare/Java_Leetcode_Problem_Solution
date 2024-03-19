a66e893db1b24702e9eb0c18aeb7c9c122bc1996
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-03-18 23:12:16 -0400

    Time: 1 ms (98.64%), Space: 41.6 MB (31.48%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,21 @@
-// import java.util.Stack;
-
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<Character>();
-
-        for (int i = 0; i < s.length(); i++) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0 ; i < s.length(); i++){
             char c = s.charAt(i);
-            if (c == '(' || c == '{' || c == '[') {
+            if(c == '(' || c == '{' || c == '['){
                 st.push(c);
-            } else {
-                if (st.isEmpty()) {
+            }else{
+                if(st.isEmpty()){
                     return false;
                 }
                 char top = st.pop();
-                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+                if(c == ')' && top != '(' || c == '}' && top != '{' || c == ']' && top != '['){
                     return false;
+                    
                 }
             }
         }
-
-        return st.isEmpty(); // Check if all opening symbols are closed
+        return st.isEmpty() ? true : false;
     }
 }
