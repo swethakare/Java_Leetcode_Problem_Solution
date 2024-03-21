47ec03badc27a2f742a06c31f36bb841f5ce9217
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-03-21 11:54:09 -0400

    Time: 0 ms (100.00%), Space: 41 MB (79.67%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -4,19 +4,13 @@ class Solution {
         ListNode prev = null;
         ListNode next = null;
         int i = 1;
-        
-        // Move cur pointer to the node at left index
         while (cur != null && i < left) {
             prev = cur;
             cur = cur.next;
             i++;
         }
-        
-        // Store the nodes before and after the sublist to be reversed
         ListNode dummy1 = prev;
         ListNode dummy2 = cur;
-        
-        // Reverse the sublist
         while (cur != null && i <= right) {
             next = cur.next;
             cur.next = prev;
@@ -24,15 +18,12 @@ class Solution {
             cur = next;
             i++;
         }
-        
-        // Update connections
-        if (dummy1 != null) {
+        if (dummy1 != null){
             dummy1.next = prev;
         } else {
-            head = prev; // If reversing starts from the head, update head to the new start of the sublist
+            head = prev; 
         }
-        dummy2.next = cur; // Connect the end of the sublist to the node after the sublist
-        
+        dummy2.next = cur; 
         return head;
     }
 }
