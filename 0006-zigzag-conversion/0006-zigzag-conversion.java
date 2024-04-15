63865d4f60d8f6603fb9d654bba8dec9843b49f2
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-04-15 12:07:52 -0400

    Time: 6 ms (49.58%), Space: 45.3 MB (30.31%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,28 +1,24 @@
 class Solution {
     public String convert(String s, int numRows) {
-        String[] st = new String[numRows];
-
-        // Initialize each element in the array to an empty string
-        for (int i = 0; i < numRows; i++) {
-            st[i] = "";
+        StringBuffer sb[] = new StringBuffer [numRows];
+        for(int i = 0 ; i<numRows; i++){
+            sb[i] = new StringBuffer();
         }
-
-        char[] ch = s.toCharArray();
-        int j = 0;
-
-        while (j < s.length()) {
-            for (int idx = 0; idx < numRows && j < ch.length; idx++) {
-                st[idx] += ch[j++];
+        char ch[] = s.toCharArray();
+        int j = 0 ;
+        while(j < s.length()){
+            for(int idx = 0; idx < numRows && j < ch.length; idx++){
+                sb[idx].append(ch[j]);
+                j++;
             }
-            for (int idx = numRows - 2; idx > 0 && j < ch.length; idx--) {
-                st[idx] += ch[j++];
+            for(int idx = numRows-2; idx > 0 && j < ch.length; idx--){
+                sb[idx].append(ch[j]);
+                j++;
             }
         }
-
-        for (int i = 1; i < numRows; i++) {
-            st[0] += st[i];
-        }
-
-        return st[0];
+      for(int i = 1; i < numRows; i++){
+          sb[0].append(sb[i]);
+      }
+        return sb[0].toString();
     }
 }
