6e98afeb5babf48a0392e5beab799926f597795a
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-05-27 19:11:39 -0400

    Time: 1 ms (81.72%), Space: 41.3 MB (66.44%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -0,0 +1,54 @@
+class item{
+    String it;
+    int cost;
+    item(String it, int cost){
+        this.it = it;
+        this.cost = cost;
+    }
+}
+class Solution {
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        Queue<item> q = new LinkedList<>();
+        q.add(new item(startGene,0));
+        HashSet<String> bnk = new HashSet<>();
+        for(int i = 0; i < bank.length; i++){
+            bnk.add(bank[i]);
+        }
+        // System.out.println(bnk);
+        int[] dis = new int[bank.length];
+        for(int i = 0; i < bank.length; i++){
+            if(bank[i] == startGene){
+                dis[i] = 0;
+                break;
+            }
+        }
+        char[] ds = {'A','C','G','T'};
+        while(!q.isEmpty()){
+            item cur = q.poll();
+            String curnode = cur.it;
+            int curcost = cur.cost;
+            char[] newstr = curnode.toCharArray();
+            for(int i = 0; i < newstr.length; i++){
+                char oldchar = newstr[i];
+               
+                for(int j = 0; j < ds.length; j++){
+                    if(newstr[i] != ds[j]){
+                        newstr[i] = ds[j];
+                        // System.out.println(String.valueOf(newstr));
+                        if(bnk.contains(String.valueOf(newstr))){
+                            String ite = new String(newstr);
+                            if(ite.equals(endGene)){ 
+                                curcost = curcost+1;
+                                return curcost;
+                            }
+                            q.add(new item(String.valueOf(newstr),curcost+1));
+                            bnk.remove(curnode);
+                        }
+                        newstr[i] = oldchar;  
+                    }
+                }  
+            }
+        }
+        return -1;
+    }
+}
