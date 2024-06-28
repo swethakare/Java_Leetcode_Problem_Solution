5d66ad26944a428ca58a214bba4ac8be0ec0880a
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-06-28 16:26:44 -0400

    Time: 31 ms (13.68%), Space: 50 MB (89.77%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,32 +1,39 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length()][text2.length()];
-        for(int i = 0; i < text1.length(); i++){
-            for(int j = 0; j < text2.length(); j++){
-                dp[i][j] = -1;
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        String [] s1 = new String[text1.length()+1];
+        String[] s2 = new String[text2.length()+1];
+        int j = 0;
+        for(int i = 1; i < s1.length; i++){
+            s1[i] = String.valueOf(text1.charAt(j));
+            j++;
+        }
+        j = 0;
+        for(int i = 1; i < s2.length; i++){
+            s2[i] = String.valueOf(text2.charAt(j));
+            j++;
+        }
+        
+        return helper(s1,s2,dp,s1.length,s2.length);
+    
+    }
+    public int helper(String[] s1, String[] s2, int[][] dp, int l, int m){
+        
+        for(int i = 1; i < l; i++){
+            for(int j = 1; j < m; j++){
+                if((s1[i]).equals(s2[j])){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 0 + Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+                
             }
         }
-        return helper(text1,text2,text1.length()-1,text2.length()-1,dp);
+
+        return dp[dp.length-1][dp[0].length-1];
+   
         
     }
-    int helper(String text1, String text2, int i1, int i2, int[][] dp){
-        if(i1 < 0 || i2 < 0){
-            return 0;
-        }
-        int match = 0;
-        int nmatch = 0;
-        if(dp[i1][i2] != -1){
-            return dp[i1][i2];
-        }
-        if(text1.charAt(i1) == text2.charAt(i2)){
-            match = 1 + helper(text1,text2,i1-1,i2-1,dp);
-        }else{
-          nmatch = 0 + Math.max(helper(text1,text2,i1-1,i2,dp),helper(text1,text2,i1,i2-1,dp));
-             }
-    
-    dp[i1][i2] = Math.max(match,nmatch);
-    return dp[i1][i2];
-    }
-        
-        
+
 }
+       
