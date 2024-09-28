5c0998d6bf143df33a21caccdd3f1f9c6c7696f9
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-09-28 00:47:41 -0400

    Time: 3 ms (37.69%), Space: 46.5 MB (90.83%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -3,15 +3,11 @@ import java.util.ArrayList;
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         ArrayList<ArrayList<Integer>> adjacencyList = new ArrayList<>();
-
-        // Initialize the adjacency list
         for (int i = 0; i < isConnected.length; i++) {
             adjacencyList.add(new ArrayList<Integer>());
         }
-
-        // Populate the adjacency list
         for (int i = 0; i < isConnected.length; i++) {
-            for (int j = i + 1; j < isConnected[i].length; j++) {
+            for (int j = 0; j < isConnected[i].length; j++) {
                 if (isConnected[i][j] == 1 && i != j) {
                     adjacencyList.get(i).add(j);
                     adjacencyList.get(j).add(i);
@@ -20,18 +16,14 @@ class Solution {
         }
         int[] visited = new int[isConnected.length];
         int cnt = 0;
-
-        // Perform DFS for each unvisited node
         for (int i = 0; i < isConnected.length; i++) {
             if (visited[i] == 0) {
                 cnt++;
                 dfs(adjacencyList, visited, i);
             }
         }
-
         return cnt;
     }
-
     public void dfs(ArrayList<ArrayList<Integer>> adj, int[] vis, int node) {
         vis[node] = 1;
         for (int it : adj.get(node)) {
