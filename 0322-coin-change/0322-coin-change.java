817a06295238b694a8d13783c52cb10a072fd949
Swetha Kare <125495436+swethakare@users.noreply.github.com>
2024-10-20 18:52:28 -0400

    Time: 12 ms (93.53%), Space: 44.2 MB (73.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,38 +1,50 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-      int[][] dp = new int[coins.length][amount+1];
-        for(int i = 0; i < coins.length; i++){
-            for(int j = 0; j < amount+1; j++){
-                dp[i][j] = -1;
+        int[] dp = new int[amount+1];
+// amount+1 because we need to check the no of minimal ways to make the target with each and every coing ; target+1 array will have the index upto targether
+        dp[0] = 0;
+        for(int i = 1; i < dp.length; i++){
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for(int coin : coins){
+            for(int i = coin; i <= amount; i++){
+                   // System.out.println(dp[i]);
+                if(dp[i - coin] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i],dp[i-coin]+1);
+                    // System.out.println("coin "+coin +" iteration "+i+" "+ dp[i]);
+                }
             }
         }
-      int ite = helper(coins, amount, 0,dp);
-      if(ite == (int) 1e9){
-          return -1;
-      }else{
-          return ite;
-      }
-    }
-    public int helper(int[] coins, int amount, int ind, int[][] dp){
-        if(ind == coins.length-1){
-            if(amount == coins[ind]){
-                return 1;
-            }else if(amount % coins[ind] == 0){
-                return amount/coins[ind];
-            }else{
-                return (int)1e9;
-            }
-        }
-        if(dp[ind][amount] != -1){
-            return dp[ind][amount];
-        }
-        int ntake = 0 + helper(coins,amount,ind+1,dp);
-        int take = Integer.MAX_VALUE;
-        if(coins[ind] <= amount){
-             take = 1 + helper(coins,amount-coins[ind],ind,dp);
-        }
-        dp[ind][amount] = Math.min(take,ntake);
-        return dp[ind][amount];
+        return dp[dp.length-1] == Integer.MAX_VALUE ? -1 : dp[dp.length-1];
     }
 }
-        
+
+// wlakthrough:
+// coin 1 iteration 1 1
+// coin 1 iteration 2 2
+// coin 1 iteration 3 3
+// coin 1 iteration 4 4
+// coin 1 iteration 5 5
+// coin 1 iteration 6 6
+// coin 1 iteration 7 7
+// coin 1 iteration 8 8
+// coin 1 iteration 9 9
+// coin 1 iteration 10 10
+// coin 1 iteration 11 11
+// coin 2 iteration 2 1
+// coin 2 iteration 3 2
+// coin 2 iteration 4 2
+// coin 2 iteration 5 3
+// coin 2 iteration 6 3
+// coin 2 iteration 7 4
+// coin 2 iteration 8 4
+// coin 2 iteration 9 5
+// coin 2 iteration 10 5
+// coin 2 iteration 11 6
+// coin 5 iteration 5 1
+// coin 5 iteration 6 2
+// coin 5 iteration 7 2
+// coin 5 iteration 8 3
+// coin 5 iteration 9 3
+// coin 5 iteration 10 2
+// coin 5 iteration 11 3
